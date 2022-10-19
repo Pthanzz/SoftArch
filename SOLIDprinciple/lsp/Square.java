658bef9fb32784000fd35b2e1e60d9261bbf58bb
Thanyalak A <69185791+Pthanzz@users.noreply.github.com>
2022-10-19 09:28:48 +0700

    Add files via upload

---
+++ SOLIDprinciple/lsp/Square.java
@@ -0,0 +1,16 @@
+public class Square extends Shape {
+    private int side;
+
+    Square(int side) {
+        this.setSide(side);
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    @Override
+    public int getArea(){
+        return side * side;
+    }
+}
